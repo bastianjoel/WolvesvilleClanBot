b37ef0e2a8716b1a8df33474c857d0f7a0dd002c
Bastian Rihm <me@bastianjoel.de>
2022-11-24 19:49:07 +0100

    update Task doc

--- src/tasks/Task.ts
+++ src/tasks/Task.ts
@@ -3,7 +3,8 @@ export interface Task {
     * Runs a task and returns its next execution
     * time or null if the task should not be
     * executed again
-    * This method should also throw on error
+    * Errors will be handled internally. When this
+    * method throws the task should not be run again
     */
   run(): Promise<Date | null>;
 }
